a80c7f3da4351058f34ae3ce1d7d431bd513f618
DexEternal <diviniterishere@yahoo.com>
2024-06-28 11:03:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    clients: {
+        token: '', // Token Du Bot
+        prefix: '', // Prefix Du Bot
+        activity: '...', 
+        embedColor: '#ff0000', // Couleur Des Embeds
+        name: 'By dexterity.py', // Texte Du Footer
+        logo: 'https://cdn.discordapp.com/avatars/1212806739666276546/71883e70768de2e50dbb6ca386e15ee0.png?size=1024', // L'image Du Footer
+        buyer: '1212806739666276546' // Ton ID
+    }
+}
